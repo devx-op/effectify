97c18071d54e043db0762c7291aa1f653f8a220b
Andres Jimenez <andresdavid@outlook.com>
2025-06-26 06:15:33 -0600

    fix vulneravility esbuild

---
+++ apps/solid-app/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+  ],
+})
