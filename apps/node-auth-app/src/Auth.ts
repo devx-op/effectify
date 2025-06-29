0bd2535e476900e9b9d34f019c511f0c5d2d43eb
Andres David Jimenez <asulbaran@theinfinitereality.com>
2025-06-28 20:42:38 -0600

    wip tanstack

--- apps/node-auth-app/src/Auth.ts
+++ apps/node-auth-app/src/Auth.ts
@@ -1,4 +1,3 @@
-import path from 'node:path'
 import { betterAuth } from 'better-auth'
 import Database from 'better-sqlite3'
 
@@ -6,6 +5,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  database: new Database(path.join(process.cwd(), 'sqlite.db')) as any,
+  database: new Database('./sqlite.db') as any,
   trustedOrigins: ['http://localhost:3000'],
 })
