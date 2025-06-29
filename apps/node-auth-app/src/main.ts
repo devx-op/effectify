7cfa038e7cd23df3e4709f96bebc89e850d8cff2
Andres David Jimenez <asulbaran@theinfinitereality.com>
2025-06-28 21:46:03 -0600

    fix biome checks

--- apps/node-auth-app/src/main.ts
+++ apps/node-auth-app/src/main.ts
@@ -1,5 +1,5 @@
 import * as NodeRuntime from '@effect/platform-node/NodeRuntime'
 import * as Layer from 'effect/Layer'
-import * as Http from './Http.js'
+import * as Http from './http.js'
 
 Layer.launch(Http.Live).pipe(NodeRuntime.runMain())
