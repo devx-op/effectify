5885cafdd979c0b3693f985c0bdcbaca4a68bf07
Andres Jimenez <andres.jimenez@golabstech.com>
2025-10-07 08:34:50 -0600

    feat(solid-query): test isolated package change for release workflow

--- packages/solid/query/src/index.ts
+++ packages/solid/query/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/tanstack-query-effect.jsx'
 export * from './lib/types.js'
 
 // Testing release workflow with changelog generation
+// This should only affect solid-query package
