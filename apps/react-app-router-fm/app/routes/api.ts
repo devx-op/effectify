d10dc4c6e3aac992f4320662280a3619a4a3179b
Andres Jimenez <andres.jimenez@golabstech.com>
2025-11-21 07:09:42 -0600

    migrate nx

--- apps/react-app-router-fm/app/routes/api.ts
+++ apps/react-app-router-fm/app/routes/api.ts
@@ -1,6 +1,6 @@
-import { makeHttpRouterHandler } from "@effectify/react-router"
+import { HttpApiHandler } from "@effectify/react-router"
 import { ApiLive } from "../lib/http.server"
 
 
-export const loader = makeHttpRouterHandler({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
-export const action = makeHttpRouterHandler({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
+export const loader = HttpApiHandler.make({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
+export const action = HttpApiHandler.make({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
