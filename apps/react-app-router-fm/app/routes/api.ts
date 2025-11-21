35ebf080fc2d6b870bfbe581d156e7251f0897bf
Andres Jimenez <andres.jimenez@golabstech.com>
2025-11-21 01:01:59 -0600

    fix http for react-router effect

---
+++ apps/react-app-router-fm/app/routes/api.ts
@@ -0,0 +1,6 @@
+import { makeHttpRouterHandler } from "@effectify/react-router"
+import { ApiLive } from "../lib/http.server"
+
+
+export const loader = makeHttpRouterHandler({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
+export const action = makeHttpRouterHandler({ apiLive: ApiLive, scalar: { baseServerURL: 'http://localhost:3000/api' } })
