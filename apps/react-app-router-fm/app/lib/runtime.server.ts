f9aa9d78e8e367e5d1fb26c8e40fdf50903186bb
Andres Jimenez <andres.jimenez@golabstech.com>
2025-11-23 18:15:50 -0600

    fix remix react router 7

--- apps/react-app-router-fm/app/lib/runtime.server.ts
+++ apps/react-app-router-fm/app/lib/runtime.server.ts
@@ -1,7 +1,8 @@
 import { Runtime } from '@effectify/react-router'
 import * as Layer from 'effect/Layer'
+import * as Logger from 'effect/Logger'
 import { AuthService } from './auth.server.js'
 
-const layers = Layer.mergeAll(AuthService.Default)
+const layers = AuthService.Default.pipe(Layer.provideMerge(Logger.pretty))
 
 export const { withLoaderEffect, withActionEffect } = Runtime.make(layers)
