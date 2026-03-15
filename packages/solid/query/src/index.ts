82cbb87ee32b043441377dc5c58053760510b301
Andres David Jimenez Sulbaran <andresdavid@outlook.com>
2026-03-15 10:50:11 -0600

    chore(solid-query): bump beta version marker

--- packages/solid/query/src/index.ts
+++ packages/solid/query/src/index.ts
@@ -1,6 +1,7 @@
 // Effect v4 Beta - Solid Query Integration
 // @beta - Part of Effect v4 beta migration
 // Published: 2026-03-15
+// Version: 0.5.13-beta.0
 export * from "./lib/internal/query-data-helpers.js"
 export * from "./lib/tanstack-query-effect.jsx"
 export * from "./lib/types.js"
