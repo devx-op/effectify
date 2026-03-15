ec2be8c5cbf4d37fa367e38b016f4bd3a7aadb2c
Andres David Jimenez Sulbaran <andresdavid@outlook.com>
2026-03-15 11:15:59 -0600

    chore(react-query): trigger beta release with config update

--- packages/react/query/src/index.ts
+++ packages/react/query/src/index.ts
@@ -2,6 +2,7 @@
 // @beta - v4.0.0-beta.31
 // Release: 2026-03-15
 // Trigger: beta release workflow
+// Repository: github.com/devx-op/effectify
 export * from "./lib/internal/query-data-helpers.js"
 export * from "./lib/tanstack-query-effect.jsx"
 export * from "./lib/types.js"
