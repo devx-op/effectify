cc2fea6497d437909d2603bdc34e21674550af95
Andres J <andresdavid@outlook.com>
2026-03-22 20:37:21 -0600

    fix: clean up formatting in GeneratorContext class definition

--- packages/prisma/src/services/generator-context.ts
+++ packages/prisma/src/services/generator-context.ts
@@ -1,9 +1,7 @@
 import type { GeneratorOptions } from "@prisma/generator-helper"
 import * as ServiceMap from "effect/ServiceMap"
 
-
-
 export class GeneratorContext extends ServiceMap.Service<
-    GeneratorContext, 
-    GeneratorOptions
+  GeneratorContext,
+  GeneratorOptions
 >()("GeneratorContext") {}
