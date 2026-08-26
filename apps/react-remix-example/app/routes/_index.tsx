a9c2fcae7471528f01e5165e0ffc085a90bf93b6
kattsushi <andresdavid@outlook.com>
2026-08-26 02:44:39 -0600

    chore(react-remix-example): remove app node

--- apps/react-remix-example/app/routes/_index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <article>
-      <h1>Welcome to Effectify</h1>
-      <p>This transitional static shell retains only its home page during consolidation.</p>
-    </article>
-  )
-}
